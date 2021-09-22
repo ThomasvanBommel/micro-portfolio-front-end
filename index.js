0b8dc79e231811544aa7f81cde3996949fc4b83a
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-09-21 22:56:01 -0300

    Modified welcome page

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const os = require("os");
 const app = express();
 
 app.all("*", (req, res) => {
-    res.send(`Welcome to <code style="color:red;">${ os.hostname() }</code> :)`);
+    res.send(`
+        <head>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <div style="width:max-content;margin:0 auto;text-align:center;">
+                <p>Welcome &#128151;</p>
+                <code style="color:red;opacity:0.5;">${ os.hostname() }</code>
+            </div>
+        </body
+    `);
 });
 
 app.listen(80, () => {
